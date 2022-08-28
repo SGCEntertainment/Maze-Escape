c7c4ef38877fad69c96380da6454b88d2a5ce55e
nikcxprojects@gmail.com <smallgamescompany@gmail.com>
2022-08-28 17:05:16 +0300

    update

---
+++ Assets/snake game fusion/Scripts/CustomNetworkInput.cs
@@ -0,0 +1,47 @@
+using Fusion;
+using Fusion.Sockets;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomNetworkInput : Fusion.Behaviour, INetworkRunnerCallbacks
+{
+	Vector2 GetInputDirection()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		return new Vector2(x, y);
+	}
+
+	public void OnInput(NetworkRunner runner, NetworkInput input)
+	{
+		var userInput = new CustomNetworkInputStruct()
+		{
+			direction = GetInputDirection()
+		};
+
+		input.Set(userInput);
+	}
+
+	public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
+	public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+	public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+	public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+	public void OnConnectedToServer(NetworkRunner runner) { }
+	public void OnDisconnectedFromServer(NetworkRunner runner) { }
+	public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+	public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+	public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+	public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+	public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+	public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+	public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+	public void OnSceneLoadDone(NetworkRunner runner) { }
+	public void OnSceneLoadStart(NetworkRunner runner) { }
+}
+
+public struct CustomNetworkInputStruct : INetworkInput
+{
+	public Vector2 direction;
+}
--- Assets/snake game fusion/Scripts/Snake Network/SnakeController.cs
+++ Assets/snake game fusion/Scripts/Snake Network/SnakeController.cs
@@ -6,25 +6,44 @@ public class SnakeController : SnakeComponent
 	[SerializeField] float speed;
 
 	public NetworkRigidbody2D Rigidbody;
-	[Networked] private SnakeInput.NetworkInputData Inputs { get; set; }
+	//[Networked] private SnakeInput.NetworkInputData Inputs { get; set; }
 	[Networked] public RoomPlayer RoomUser { get; set; }
 
-	public override void FixedUpdateNetwork()
-	{
-		base.FixedUpdateNetwork();
+    //public override void FixedUpdateNetwork()
+    //{
+    //	base.FixedUpdateNetwork();
 
-		if (GetInput(out SnakeInput.NetworkInputData input))
-		{
-			Inputs = input;
-		}
+    //	if (GetInput(out SnakeInput.NetworkInputData input))
+    //	{
+    //		Inputs = input;
+    //	}
 
-		Move(Inputs);
-	}
+    //	Move(Inputs);
+    //}
 
-	private void Move(SnakeInput.NetworkInputData input)
-	{
-		Rigidbody.Rigidbody.position += Runner.DeltaTime * speed * input.direction;
-	}
+    public override void FixedUpdateNetwork()
+    {
+        base.FixedUpdateNetwork();
+
+        Vector2 direction = Vector2.zero;
+        if (GetInput(out CustomNetworkInputStruct input))
+        {
+            direction = input.direction.normalized;
+        }
+
+        Move(direction);
+    }
+
+    //   private void Move(SnakeInput.NetworkInputData input)
+    //{
+    //	Rigidbody.Rigidbody.position += Runner.DeltaTime * speed * input.direction;
+    //}
+    private void Move(Vector2 direction)
+    {
+        Rigidbody.Rigidbody.MovePosition(Rigidbody.Rigidbody.position + Runner.DeltaTime * speed * direction);
+        //Rigidbody.Rigidbody.position += Runner.DeltaTime * speed * direction;
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
