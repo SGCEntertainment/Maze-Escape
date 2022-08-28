ef597e89abb990b917ccf9d2a0010a329413447b
nikcxprojects@gmail.com <smallgamescompany@gmail.com>
2022-08-28 23:49:42 +0300

    update

--- Assets/Learning Files/Scripts/LearningPlayer2D.cs
+++ Assets/Learning Files/Scripts/LearningPlayer2D.cs
@@ -1,9 +1,13 @@
 using Fusion;
+using UnityEngine;
 
 public class LearningPlayer2D : NetworkBehaviour
 {
     private NetworkRigidbody2D nr2d;
     public float Speed;
+
+    [Networked]
+    public Vector2 MovementDirection { get; set; }
 
     private void Awake()
     {
@@ -12,10 +16,21 @@ public class LearningPlayer2D : NetworkBehaviour
 
     public override void FixedUpdateNetwork()
     {
+        Vector3 direction;
         if (GetInput(out LearningNetworkInputData data))
         {
-            data.direction2D.Normalize();
-            nr2d.Rigidbody.MovePosition(nr2d.Rigidbody.position + Runner.DeltaTime * Speed * data.direction2D);
+            Vector2 moveDirection = data.direction2D;
+            direction = moveDirection;
+
+            direction = direction.normalized;
+            MovementDirection = direction;
         }
+        else
+        {
+            direction = MovementDirection;
+        }
+
+        Vector2 direction2d = new Vector2(direction.x, direction.y);
+        nr2d.Rigidbody.MovePosition(nr2d.Rigidbody.position + Runner.DeltaTime * Speed * direction2d);
     }
 }
