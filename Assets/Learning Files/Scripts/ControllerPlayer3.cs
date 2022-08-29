4b3dc20d7aee8f7ebd81651e84666fe30ec2fa57
nikcxprojects@gmail.com <smallgamescompany@gmail.com>
2022-08-29 16:33:22 +0300

    update

--- Assets/Learning Files/Scripts/ControllerPlayer3.cs
+++ Assets/Learning Files/Scripts/ControllerPlayer3.cs
@@ -6,7 +6,16 @@ public class ControllerPlayer3 : NetworkBehaviour
     protected NetworkRigidbody2D _nrb2d;
 
     [Networked]
-    public Vector3 MovementDirection { get; set; }
+    public Vector2 MovementDirection { get; set; }
+
+    [HideInInspector]
+    public Vector2 Velocity { get; set; }
+
+    public float acceleration = 10.0f;
+    public float braking = 10.0f;
+    public float maxSpeed = 2.0f;
+
+
 
     public float Speed = 6f;
 
@@ -35,29 +44,29 @@ public class ControllerPlayer3 : NetworkBehaviour
             return;
         }
 
-        Vector3 direction;
+        Vector2 direction;
         if (GetInput(out LearningNetworkInputData input))
         {
             direction = default;
 
             if (input.IsDown(NetworkInputPrototype.BUTTON_FORWARD))
             {
-                direction += Vector3.forward;
+                direction += Vector2.up;
             }
 
             if (input.IsDown(NetworkInputPrototype.BUTTON_BACKWARD))
             {
-                direction -= Vector3.forward;
+                direction -= Vector2.up;
             }
 
             if (input.IsDown(NetworkInputPrototype.BUTTON_LEFT))
             {
-                direction -= Vector3.right;
+                direction -= Vector2.right;
             }
 
             if (input.IsDown(NetworkInputPrototype.BUTTON_RIGHT))
             {
-                direction += Vector3.right;
+                direction += Vector2.right;
             }
 
             direction = direction.normalized;
@@ -72,15 +81,33 @@ public class ControllerPlayer3 : NetworkBehaviour
 
         if (_nrb2d && !_nrb2d.Rigidbody.isKinematic)
         {
-            Vector2 direction2d = new Vector2(direction.x, direction.y + direction.z);
+            Vector2 direction2d = new Vector2(direction.x, direction.y);
             _nrb2d.Rigidbody.AddForce(direction2d * Speed);
         }
         else
         {
-            
-            direction = new Vector3(direction.x, direction.z, 0);
-            _nrb2d.TeleportToPosition(transform.position + Runner.DeltaTime * Speed * direction, Vector3.zero, true);
-            //transform.position += (direction * Speed * Runner.DeltaTime);
+            var deltaTime = Runner.DeltaTime;
+            var previousPos = transform.position;
+            var moveVelocity = Velocity;
+
+            var horizontalVel = default(Vector2);
+            horizontalVel.x = moveVelocity.x;
+            horizontalVel.y = moveVelocity.y;
+
+            if (direction == default)
+            {
+                horizontalVel = Vector3.Lerp(horizontalVel, default, braking * deltaTime);
+            }
+            else
+            {
+                horizontalVel = Vector3.ClampMagnitude(horizontalVel + direction * acceleration * deltaTime, maxSpeed);
+            }
+
+            moveVelocity.x = horizontalVel.x;
+            moveVelocity.y = horizontalVel.y;
+
+            transform.position +=  new Vector3(moveVelocity.x, moveVelocity.y, 0) * deltaTime;
+            Velocity = (transform.position - previousPos) * Runner.Simulation.Config.TickRate;
         }
     }
 }
